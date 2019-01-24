f7fdb2ac373755425ecfa78834946ee5fc7cc912
Steven Olufemi-OluKa <Steven.Olufemi-OluKanni@jet2.com>
2019-01-24 11:07:11 +0000

    Add project files.

---
+++ SimonWebAppCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SimonWebAppCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
